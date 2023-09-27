c4d66818ee539b5196c5db55af46e81d733ea77e
IvanSmirnov <includingivansmirnov@gmail.com>
2023-09-27 23:51:00 +0500

    1

--- StudentHunter.API/Controllers/WeatherForecastController.cs
+++ StudentHunter.API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing"
+        "Freezing", "Bracing", "Chilly"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
